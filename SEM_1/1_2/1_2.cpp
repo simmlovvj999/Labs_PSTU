6c5e325b86647102c17e2b878a0d23d7f48f256e
simmlovvj999 <simmlovvj@gmail.com>
2025-12-16 13:39:57 +0500

    Update 1_2.cpp

--- SEM_1/1_2/1_2.cpp
+++ SEM_1/1_2/1_2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	float a, b;
+	std::cin >> a >> b;
+	if (b == 0)
+	{
+		std::cout << "error, b = 0.";
+	}
+	else
+	{
+		std::cout << a / b;
+	}
+	return 0;
+}
 
