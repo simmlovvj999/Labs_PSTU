a1026a3b7b6a34342748162c3ba1cbb1f09af19d
simmlovvj999 <simmlovvj@gmail.com>
2025-12-16 13:37:34 +0500

    Update 1_1.cpp

--- SEM_1/1_1/1_1.cpp
+++ SEM_1/1_1/1_1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	int a, b, c, max;
+	std::cin >> a >> b >> c;
+	if (a >= b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	if (c > max)
+	{
+		max = c;
+	}
+	std::cout << max << std::endl;
+	return 0;
+}
 
