3765f8d7888fad0f2fe0c1d18a840f183b5dfc52
simmlovvj999 <simmlovvj@gmail.com>
2025-12-23 14:44:49 +0500

    Update ptr_4.cpp

--- SEM_1/ptr_4/ptr_4.cpp
+++ SEM_1/ptr_4/ptr_4.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>  
+using namespace std;
+int main(){ 
+    int N; 
+    float a, max, min; 
+    float *ptr_max = &max; 
+    float *ptr_min = &min;
+    cin >> N; 
+    cin >> *ptr_max; 
+    *ptr_min = *ptr_max; 
+    for (int i = 1; i < N; i++){
+        cin >> a; 
+        if(a > *ptr_max) {*ptr_max = a;}
+        if (a < *ptr_min) {*ptr_min = a;}
+    }
+    cout << "max = " << *ptr_max << " min = " << *ptr_min << endl;
+}
