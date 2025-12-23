4998a08bc80981c306ab94ad8bd0db4bdd39e800
simmlovvj999 <simmlovvj@gmail.com>
2025-12-23 14:43:35 +0500

    Update ptr_3.cpp

--- SEM_1/ptr_3/ptr_3.cpp
+++ SEM_1/ptr_3/ptr_3.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream> 
+using namespace std;
+int main(){ 
+    float a, b, sum; 
+    cin >> a >> b; 
+    float *ptr_1 = &a; 
+    float *ptr_2 = &b; 
+    sum = *ptr_1 + *ptr_2; 
+    cout << "a + b = " << sum << endl; 
+    return 0;
+}
