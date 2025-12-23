c422d422e2704c280e278b8e786dad8232871e76
simmlovvj999 <simmlovvj@gmail.com>
2025-12-23 14:45:57 +0500

    Update ptr_5.cpp

--- SEM_1/ptr_5/ptr_5.cpp
+++ SEM_1/ptr_5/ptr_5.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>  
+using namespace std;
+int main(){
+    int N, factorial = 1; 
+    cin >> N; 
+    int *ptr_1 = &N; 
+    int *ptr_fact = &factorial; 
+    for (int i = 1; i <= *ptr_1; i++) {
+        *ptr_fact *= i;
+    }
+    cout << factorial; 
+}
