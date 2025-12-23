56842953ce7bdf05e56eeeeac010bc16f58563e1
simmlovvj999 <simmlovvj@gmail.com>
2025-12-23 14:41:08 +0500

    Update ptr_1.cpp

--- SEM_1/ptr_1/ptr_1.cpp
+++ SEM_1/ptr_1/ptr_1.cpp
@@ -1,1 +1,13 @@
 
+#include <iostream> 
+using namespace std; 
+int main(){
+    int N;
+    cout << "Введите изначальное число: "; 
+    cin >> N;
+    int *ptr = &N; 
+    cout << "Введите измененное число: "; 
+    cin >> *ptr; 
+    cout << N << endl;
+    return 0;
+}
