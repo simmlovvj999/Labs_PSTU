e3d7f589ef1aaacbc06d0284b286f17e979e30a5
simmlovvj999 <simmlovvj@gmail.com>
2026-05-20 00:30:34 +0500

    Update 3.12.cpp

--- SEM_2/3/3.12/3.12.cpp
+++ SEM_2/3/3.12/3.12.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int range_min = -100;
+    int range_max = 100;
+    int N;
+    cin >> N;
+    int a[N][N];
+    
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j<N; j++){
+               a[i][j] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;  
+               cout << a[i][j] << " ";
+        }
+        cout << endl; 
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        a[i][N-1-i] = 0;
+    }
 
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        { 
+            cout << a[i][j] << " "; 
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
