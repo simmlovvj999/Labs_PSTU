cc84c6e99906234742c99d5f5037f975e8ec6cfe
simmlovvj999 <simmlovvj@gmail.com>
2026-05-20 00:47:14 +0500

    Update l6.cpp

--- SEM_2/lab_6/l6.cpp
+++ SEM_2/lab_6/l6.cpp
@@ -1,1 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
 
+const int MAX = 256;
+
+
+bool isOnlyDigits(char word[]) {
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (!isdigit(word[i])) return false;
+    }
+    return true;
+}
+
+
+void toIdentifier(char word[]) {
+    if (!isalpha(word[0]) && word[0] != '_') {
+        int len = strlen(word);
+        for (int i = len; i >= 0; i--) {
+            word[i + 1] = word[i];
+        }
+        word[0] = '_';
+    }
+    
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (!isalnum(word[i]) && word[i] != '_') {
+            word[i] = '_';
+        }
+    }
+}
+
+int main() {
+    char str[MAX];
+    
+    cout << "Введите строку (до 255 символов, заканчивается точкой):" << endl;
+    cin.getline(str, MAX);
+    
+    int len = strlen(str);
+    if (len > 0 && str[len - 1] == '.') {
+        str[len - 1] = '\0';
+    }
+    
+    char words[50][MAX]; 
+    int wordCount = 0;
+    
+    char* token = strtok(str, " ");
+    while (token != NULL) {
+        strcpy(words[wordCount], token);
+        wordCount++;
+        token = strtok(NULL, " ");
+    }
+    
+    char result[MAX] = "";
+    
+    for (int i = 0; i < wordCount; i++) {
+        if (isOnlyDigits(words[i])) {
+            continue;
+        }
+        
+        toIdentifier(words[i]);
+        
+        if (strlen(result) > 0) {
+            strcat(result, " ");
+        }
+        strcat(result, words[i]);
+    }
+    
+    cout << "\nПреобразованная строка:" << endl;
+    cout << result << endl;
+    
+    return 0;
+}
