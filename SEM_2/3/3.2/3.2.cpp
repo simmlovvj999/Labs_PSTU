ae961134a43af0c3d479e0f40d69c5143000c43b
simmlovvj999 <simmlovvj@gmail.com>
2026-05-20 00:19:45 +0500

    Update 3.2.cpp

--- SEM_2/3/3.2/3.2.cpp
+++ SEM_2/3/3.2/3.2.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int N = 11;
+    int tmp, p, q;
+    int a[N] = { 34, 67, 89, 100, 20, 45, 31, 78, 90, 25, 57 };
+    cout << "введите p и q" << endl;
+    cin >> p >> q;
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    while (p < q)
+    {
+        tmp = a[p];
+        a[p] = a[q];
+        a[q] = tmp;
+        p++;
+        q--;
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
