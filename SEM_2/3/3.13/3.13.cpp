95e65ae264e8ff07f02e14304ddc4642b70cd0be
simmlovvj999 <simmlovvj@gmail.com>
2026-05-20 00:32:34 +0500

    Update 3.13.cpp

--- SEM_2/3/3.13/3.13.cpp
+++ SEM_2/3/3.13/3.13.cpp
@@ -1,1 +1,42 @@
+#include <iostream> 
+#include <cstdlib>
+#include <ctime>
+using namespace std; 
+int main(){
+    srand(time(0));
+    int range_min = -1000;
+    int range_max = 1000;
+    int N;
+    cin >> N;
+    int a[N][N];
+    int tmp;
+    int k = 1; 
+    cin >> k; 
+    if (k >= N) { k = N-1;}
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j<N; j++){
+               a[i][j] = ((double)rand() / RAND_MAX) * (range_max - range_min) + range_min;  
+               cout << a[i][j] << " ";
+        }
+        cout << endl; 
+    }
 
+    for (int i = 0; i < N-1; i++){
+        for (int j = 0; j < N - 1 - i; j++){
+            if (a[k][j] > a[k][k+1]){
+                for (int r =0; r < N; r++){ 
+                    tmp = a[r][j]; 
+                    a[r][j] = a[r][j+1]; 
+                    a[r][j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++){
+        for(int j = 0; j<N; j++){
+            cout << a[i][j] << " "; 
+        }
+        cout << endl;
+    }
+}
